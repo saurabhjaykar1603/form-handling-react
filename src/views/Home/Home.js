a0ef3010436bee0da3b7b8cf8be8c37743faa883
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-29 21:26:45 +0530

    learned : formed handling : input type radio

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,10 +4,13 @@ function Home() {
   // input type text form  handle
   const [fullName, setFullName] = useState("");
 
+  //   form handling radio button
+  const [genders, setGenders] = useState("male");
+
   return (
     <>
       <form>
-        <h5 style={{textAlign: "center"}}>Form Handling in React</h5>
+        <h5 style={{ textAlign: "center" }}>Form Handling in React</h5>
         <div className="form-card">
           <input
             type="text"
@@ -18,6 +21,36 @@ function Home() {
             value={fullName}
             className="input-text"
           />
+          {/* // form handling radio button */}
+
+          <div className="d-block mx-auto input-radio">
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setGenders(e.target.value);
+                }
+              }}
+              checked = {genders==="male"}
+            />{" "}
+            <span>male</span>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              onChange={(e) => {
+                if (e.target.checked) {
+                    setGenders(e.target.value);
+                  }
+              }}
+              checked = {genders==="fmale"}
+
+            />{" "}
+            <span>female</span>
+          </div>
+            <p className="input-radio-p text-center"> Gender: {genders}</p>
         </div>
       </form>
     </>
